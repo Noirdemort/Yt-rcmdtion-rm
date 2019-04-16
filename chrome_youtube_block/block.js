d7405199dca0571e2c2545db7181fa991a849b62
Noirdemort <45682557+Noirdemort@users.noreply.github.com>
2019-04-16 16:04:57 +0530

    added files for safari and chrome

---
+++ chrome_youtube_block/block.js
@@ -0,0 +1,9 @@
+// blocks recommendation
+document.getElementsByClassName("style-scope ytd-page-manager")[0].innerHTML = ""
+document.getElementsByClassName("style-scope ytd-page-manager")[1].innerHTML = ""
+document.getElementsByClassName("style-scope ytd-page-manager")[2].innerHTML = ""
+// blocks apps on side
+document.getElementById("player-ads").innerHTML = ""
+
+// blocks suggestion while playing a video
+document.getElementsByTagName("ytd-watch-next-secondary-results-renderer")[0].innerHTML = ""
